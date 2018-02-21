8ba0c3247b5604e97adfbfc6fdfb03b01942c359
avi-007 <33357801+avi-007@users.noreply.github.com>
2018-02-21 22:11:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/article1.html', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
 
-app.get('/ui/article2.html',function(req res){
+app.get('/ui/article2.html', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui' , 'article2.html'));
 });
 
